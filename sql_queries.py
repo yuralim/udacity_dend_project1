55f72e9e6462f9e14e59e41a61f9702de2ce7d50
ylim <ylim@equilar.com>
2019-04-13 23:18:10 -0700

    Fix PEP8 errors

--- sql_queries.py
+++ sql_queries.py
@@ -11,13 +11,13 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 songplay_table_create = ("""
 CREATE TABLE songplays(
     songplay_id serial primary key,
-    start_time timestamp, 
-    user_id varchar, 
-    level varchar, 
-    song_id varchar, 
-    artist_id varchar, 
+    start_time timestamp,
+    user_id varchar,
+    level varchar,
+    song_id varchar,
+    artist_id varchar,
     session_id int,
-    location varchar, 
+    location varchar,
     user_agent text
 )
 """)
@@ -25,19 +25,19 @@ CREATE TABLE songplays(
 user_table_create = ("""
 CREATE TABLE users(
     user_id int primary key,
-    first_name varchar, 
-    last_name varchar, 
-    gender varchar, 
+    first_name varchar,
+    last_name varchar,
+    gender varchar,
     level varchar
 )
 """)
 
 song_table_create = ("""
 CREATE TABLE songs(
-    song_id varchar primary key, 
+    song_id varchar primary key,
     title varchar,
     artist_id varchar,
-    year int, 
+    year int,
     duration float
 )
 """)
@@ -45,21 +45,21 @@ CREATE TABLE songs(
 artist_table_create = ("""
 CREATE TABLE artists(
     artist_id varchar primary key,
-    name varchar, 
-    location varchar, 
-    lattitude float, 
+    name varchar,
+    location varchar,
+    lattitude float,
     longitude float
 )
 """)
 
 time_table_create = ("""
 CREATE TABLE time(
-    start_time timestamp primary key, 
+    start_time timestamp primary key,
     hour int,
-    day int, 
-    week int, 
-    month int, 
-    year int, 
+    day int,
+    week int,
+    month int,
+    year int,
     weekday int
 )
 """)
@@ -67,7 +67,15 @@ CREATE TABLE time(
 # INSERT RECORDS
 
 songplay_table_insert = ("""
-INSERT INTO songplays(start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+INSERT INTO songplays(
+    start_time,
+    user_id,
+    level,
+    song_id,
+    artist_id,
+    session_id,
+    location,
+    user_agent)
 VALUES(%s, %s, %s, %s, %s, %s, %s, %s)
 """)
 
@@ -99,11 +107,24 @@ ON CONFLICT (start_time) DO NOTHING
 # FIND SONGS
 
 song_select = ("""
-select s.song_id songid, s.artist_id artistid from songs s inner join artists a on s.artist_id = a.artist_id
+select s.song_id songid, s.artist_id artistid
+from songs s inner join artists a on s.artist_id = a.artist_id
 where s.title = %s and a.name = %s and s.duration = %s
 """)
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+create_table_queries = [
+    songplay_table_create,
+    user_table_create,
+    song_table_create,
+    artist_table_create,
+    time_table_create
+]
+drop_table_queries = [
+    songplay_table_drop,
+    user_table_drop,
+    song_table_drop,
+    artist_table_drop,
+    time_table_drop
+]
